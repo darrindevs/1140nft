47ffd64fb0e135161b800e1d9f742f0108abf9c6
Zaino <hello@darrin.dev>
2022-02-07 15:15:35 -0800

    add home sections

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,8 @@ import { Container } from 'react-bootstrap'
 import { Row } from 'react-bootstrap'
 import { Col } from 'react-bootstrap'
 import { Button } from 'react-bootstrap'
-import { Media } from 'react-bootstrap'
+//import { Media } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
 
 
@@ -53,11 +54,11 @@ const Layout = ({ children }) => {
         <Container>
           <main>
             {/* Row 1 */}
-            <div className="section-1">
-              <Row className="row-1">
+            <div className="Hero">
+              <Row>
                 <Col>
               <h1>Fractionalized <span className="blue">Real World</span> Real Estate Investing</h1>
-              <h2>Sub Headline</h2>
+              <h2>Hero</h2>
               <p>Hexagon brunch vape bitters actually post-ironic. YOLO vaporware flannel kogi, subway tile trust fund direct trade chicharrones vegan kitsch fashion axe bushwick. Hexagon farm-to-table disrupt blog intelligentsia flannel sriracha gentrify meggings. Tacos put a bird on it offal, iceland enamel pin 90's next level readymade seitan narwhal brooklyn polaroid copper mug try-hard gochujang. Mlkshk selfies YOLO, lyft meditation brooklyn hoodie live-edge viral man bun. Af farm-to-table shaman, subway tile la croix thundercats pickled.</p>
                   <span> <Button variant="outline-primary">Follow Us</Button>{' '}</span>
                   <span> <Button variant="outline-primary">Join Discord</Button>{' '}</span>
@@ -67,9 +68,9 @@ const Layout = ({ children }) => {
             </div>
            
              {/* Row 2 */}
-             <div className="section-1">
-              <Row className="row-2">
-               <h2>A Heading</h2>
+             <div className="block" id="Intro">
+              <Row>
+               <h2>Intro</h2>
                 <Col>
                   <p>
                   Poke hexagon keytar post-ironic echo park chambray humblebrag coloring book kitsch woke. Mustache whatever man braid salvia poke meditation kickstarter cred, locavore try-hard pork belly cornhole tbh roof party sriracha. 
@@ -82,34 +83,42 @@ const Layout = ({ children }) => {
                 </Col>
               </Row>
              </div>
-             
-             {/* Row 3 - the yellow box */}
-            <div className="box-1">
-            </div>
-             {/* Row 5 */}
-             <div className="section-1">
-              <Row className="box-2">
-                <h2>A Heading</h2>
-                <Col>
-                  <h6><span className="me-3">⭐️</span> media object </h6>
-                  <h6><span className="me-3">⭐️</span> media object </h6>
-                  <h6><span className="me-3">⭐️</span> media object </h6>
-                  <h6><span className="me-3">⭐️</span> media object </h6>
-                </Col>
-                <Col>
-                  <h6><span className="me-3">⭐️</span> media object </h6>
-                  <h6><span className="me-3">⭐️</span> media object </h6>
-                  <h6><span className="me-3">⭐️</span> media object </h6>
-                <span> <Button variant="outline-primary">Do Something</Button>{' '}</span>
-                </Col>
-              </Row>
-             </div>
-             <div>
+             <div className="block" id="mint">
                <Row>
-                 <h2>A Heading</h2>
-                 
+                <h2>About Mint</h2>
                </Row>
              </div>
+             <div className="block" id="community">
+               <Row>
+                <h2>Community</h2>
+               </Row>
+             </div>
+             <div className="block" id="roadmap">
+               <Row>
+                <h2>Roadmap</h2>
+               </Row>
+             </div>
+             <div className="block" id="Team">
+               <Row>
+                <h2>Team</h2>
+               </Row>
+             </div>
+             <div className="block" id="mint-cta">
+               <Row>
+                <h2>Mint CTA</h2>
+               </Row>
+             </div>
+             <div className="block" id="value-add">
+               <Row>
+                <h2>Value Add</h2>
+               </Row>
+             </div>
+             <div className="block" id="faq">
+               <Row>
+                <h2>FAQs</h2>
+               </Row>
+             </div>
+             
             
             
            
@@ -117,6 +126,7 @@ const Layout = ({ children }) => {
           </main>
         </Container>
         <Footer/>
+        
     </>
   )
 }
