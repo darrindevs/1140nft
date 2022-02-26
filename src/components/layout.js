6ce74058da7b48523cc2c9f46be9136b766d7537
Zaino <hello@darrin.dev>
2022-02-26 10:45:36 -0800

    update image blocks

--- src/components/layout.js
+++ src/components/layout.js
@@ -70,36 +70,29 @@ const Layout = ({ children }) => {
 
 
              <Row className="justify-content-md-center">
-             <div class="row row-cols-1 row-cols-md-6">
-             <img id="p1" align="" src="https://via.placeholder.com/150/0000FF/808080" width={300} height={300} />
-                 <img id="p2" align="" src="https://via.placeholder.com/150/FF0000/FFFFFF" width={450} height={450} />
-                 <img id="p3" align="" src="https://via.placeholder.com/150/FFFF00" width={400} height={250} />
-                 <img id="p4" align="" src="https://via.placeholder.com/150/000000" width={300} height={500} />
-                 <img id="p5" align="" src="https://via.placeholder.com/150/0000FF/808080" width={300} height={300} />
-                 <img id="p6" align="" src="https://via.placeholder.com/150/FF0000/FFFFFF" width={400} height={400} />
+             <div class="row row-cols-1 row-cols-md-3 mb-5">
+                  <img id="p5" align="" src="https://zno.s3.us-west-1.amazonaws.com/la2.jpeg" width={300} height={300} />    
+                 
+                 {/*<img id="p2" align="" src="https://via.placeholder.com/150/FF0000/FFFFFF" width={450} height={450} />*/}
+                 <img id="p3" align="" src="https://zno.s3.us-west-1.amazonaws.com/blueprint1.jpeg" width={300} height={300} />
+                 <img id="p1" align="" src="https://zno.s3.us-west-1.amazonaws.com/sofi1.jpeg" width={300} height={300} />
+                 
+                 
+               </div>
+               <div class="row row-cols-1 row-cols-md-4 justify-content-center">
+                 
+                 <img id="p4" align="" src="https://zno.s3.us-west-1.amazonaws.com/glucas1.jpeg" width={300} height={300} />
+                 
+                 <img id="p6" align="" src="https://zno.s3.us-west-1.amazonaws.com/clippers1.jpeg" width={300} height={300} />
                </div>
 
 
              
              </Row>
+             
              <Row className="justify-content-md-center">
                <Col md={{ span: 4, offset: 0 }}>
-                 <div class="card card-map">
-                   <div class="card-body">
-                     <h2>Heading</h2>
-                     <p>Hexagon brunch vape bitters actually post-ironic. YOLO vaporware flannel kogi, subway tile trust fund direct trade chicharrones vegan kitsch fashion axe bushwick. Hexagon farm-to-table disrupt blog intelligentsia flannel sriracha gentrify meggings. Tacos put a bird on it offal, iceland enamel pin 90's next level readymade seitan narwhal brooklyn polaroid copper mug try-hard gochujang. Mlkshk selfies YOLO, lyft meditation brooklyn hoodie live-edge viral man bun. Af farm-to-table shaman, subway tile la croix thundercats pickled.</p>
-                   </div>
-                 </div>
-               </Col>
-             </Row>
-             <Row className="justify-content-md-center">
-               <Col md={{ span: 4, offset: 0 }}>
-                 <div class="card card-map">
-                   <div class="card-body">
-                     <h2>Heading</h2>
-                     <p>Hexagon brunch vape bitters actually post-ironic. YOLO vaporware flannel kogi, subway tile trust fund direct trade chicharrones vegan kitsch fashion axe bushwick. Hexagon farm-to-table disrupt blog intelligentsia flannel sriracha gentrify meggings. Tacos put a bird on it offal, iceland enamel pin 90's next level readymade seitan narwhal brooklyn polaroid copper mug try-hard gochujang. Mlkshk selfies YOLO, lyft meditation brooklyn hoodie live-edge viral man bun. Af farm-to-table shaman, subway tile la croix thundercats pickled.</p>
-                   </div>
-                 </div>
+                 foo
                </Col>
              </Row>
              
