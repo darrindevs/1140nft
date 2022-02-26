1cc7bf31f2145b5bb7433ba094177787f77aade1
Zaino <hello@darrin.dev>
2022-02-25 22:15:38 -0800

    update block of images

--- src/components/layout.js
+++ src/components/layout.js
@@ -60,19 +60,26 @@ const Layout = ({ children }) => {
                  <p>Hexagon brunch vape bitters actually post-ironic. YOLO vaporware flannel kogi, subway tile trust fund direct trade chicharrones vegan kitsch fashion axe bushwick. Hexagon farm-to-table disrupt blog intelligentsia flannel sriracha gentrify meggings. Tacos put a bird on it offal, iceland enamel pin 90's next level readymade seitan narwhal brooklyn polaroid copper mug try-hard gochujang. Mlkshk selfies YOLO, lyft meditation brooklyn hoodie live-edge viral man bun. Af farm-to-table shaman, subway tile la croix thundercats pickled.</p>
                </Col>
              </Row>
+
+             
+
+
+
+
+
+
+
              <Row className="justify-content-md-center">
-               <div className="">
-                 
-                 <img id="p1" align="" src="https://via.placeholder.com/150/0000FF/808080" width={300} height={300} />
+             <div class="row row-cols-1 row-cols-md-6">
+             <img id="p1" align="" src="https://via.placeholder.com/150/0000FF/808080" width={300} height={300} />
                  <img id="p2" align="" src="https://via.placeholder.com/150/FF0000/FFFFFF" width={450} height={450} />
                  <img id="p3" align="" src="https://via.placeholder.com/150/FFFF00" width={400} height={250} />
                  <img id="p4" align="" src="https://via.placeholder.com/150/000000" width={300} height={500} />
                  <img id="p5" align="" src="https://via.placeholder.com/150/0000FF/808080" width={300} height={300} />
                  <img id="p6" align="" src="https://via.placeholder.com/150/FF0000/FFFFFF" width={400} height={400} />
-                 
-                
-                 
                </div>
+
+
              
              </Row>
              <Row className="justify-content-md-center">
