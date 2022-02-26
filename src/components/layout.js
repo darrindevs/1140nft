0000671fd72cafa867cb9d160b11ce5c262516b9
Zaino <hello@darrin.dev>
2022-02-25 21:40:12 -0800

    start cbclone layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -51,39 +51,29 @@ const Layout = ({ children }) => {
       {/* Custom Navbar */}
       {NavLight}
       {/* Container 1 */}
-        <Container fluid className="container-1">
+        <Container fluid className="">
           <main>
-          <Row className="justify-content-md-center">
+          
+             <Row className="justify-content-md-center">
                <Col md={{ span: 4, offset: 0 }}>
-                 <div class="card card-map">
-                   <div class="card-body">
-                     <h1>Fractionalized <span className="blue">Real World</span> Real Estate Investing</h1>
-                     <p>Hexagon brunch vape bitters actually post-ironic. YOLO vaporware flannel kogi, subway tile trust fund direct trade chicharrones vegan kitsch fashion axe bushwick. Hexagon farm-to-table disrupt blog intelligentsia flannel sriracha gentrify meggings. Tacos put a bird on it offal, iceland enamel pin 90's next level readymade seitan narwhal brooklyn polaroid copper mug try-hard gochujang. Mlkshk selfies YOLO, lyft meditation brooklyn hoodie live-edge viral man bun. Af farm-to-table shaman, subway tile la croix thundercats pickled.</p>
-                     <span> <Button variant="outline-primary">Follow Us</Button>{' '}</span>
-                     <span> <Button variant="outline-primary">Join Discord</Button>{' '}</span>
-                   </div>
-                 </div>
+                 <h1>Hero Heading</h1>
+                 <p>Hexagon brunch vape bitters actually post-ironic. YOLO vaporware flannel kogi, subway tile trust fund direct trade chicharrones vegan kitsch fashion axe bushwick. Hexagon farm-to-table disrupt blog intelligentsia flannel sriracha gentrify meggings. Tacos put a bird on it offal, iceland enamel pin 90's next level readymade seitan narwhal brooklyn polaroid copper mug try-hard gochujang. Mlkshk selfies YOLO, lyft meditation brooklyn hoodie live-edge viral man bun. Af farm-to-table shaman, subway tile la croix thundercats pickled.</p>
                </Col>
              </Row>
              <Row className="justify-content-md-center">
-               <Col md={{ span: 4, offset: 0 }}>
-                 <div class="card card-map">
-                   <div class="card-body">
-                     <h2>Heading</h2>
-                     <p>Hexagon brunch vape bitters actually post-ironic. YOLO vaporware flannel kogi, subway tile trust fund direct trade chicharrones vegan kitsch fashion axe bushwick. Hexagon farm-to-table disrupt blog intelligentsia flannel sriracha gentrify meggings. Tacos put a bird on it offal, iceland enamel pin 90's next level readymade seitan narwhal brooklyn polaroid copper mug try-hard gochujang. Mlkshk selfies YOLO, lyft meditation brooklyn hoodie live-edge viral man bun. Af farm-to-table shaman, subway tile la croix thundercats pickled.</p>
-                   </div>
-                 </div>
-               </Col>
-             </Row>
-             <Row className="justify-content-md-center">
-               <Col md={{ span: 4, offset: 0 }}>
-                 <div class="card card-map">
-                   <div class="card-body">
-                     <h2>Heading</h2>
-                     <p>Hexagon brunch vape bitters actually post-ironic. YOLO vaporware flannel kogi, subway tile trust fund direct trade chicharrones vegan kitsch fashion axe bushwick. Hexagon farm-to-table disrupt blog intelligentsia flannel sriracha gentrify meggings. Tacos put a bird on it offal, iceland enamel pin 90's next level readymade seitan narwhal brooklyn polaroid copper mug try-hard gochujang. Mlkshk selfies YOLO, lyft meditation brooklyn hoodie live-edge viral man bun. Af farm-to-table shaman, subway tile la croix thundercats pickled.</p>
-                   </div>
-                 </div>
-               </Col>
+               <div className="">
+                 
+                 <img id="p1" align="" src="https://via.placeholder.com/150/0000FF/808080" width={300} height={300} />
+                 <img id="p2" align="" src="https://via.placeholder.com/150/FF0000/FFFFFF" width={450} height={450} />
+                 <img id="p3" align="" src="https://via.placeholder.com/150/FFFF00" width={400} height={250} />
+                 <img id="p4" align="" src="https://via.placeholder.com/150/000000" width={300} height={500} />
+                 <img id="p5" align="" src="https://via.placeholder.com/150/0000FF/808080" width={300} height={300} />
+                 <img id="p6" align="" src="https://via.placeholder.com/150/FF0000/FFFFFF" width={400} height={400} />
+                 
+                
+                 
+               </div>
+             
              </Row>
              <Row className="justify-content-md-center">
                <Col md={{ span: 4, offset: 0 }}>
