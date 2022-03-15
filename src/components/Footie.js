0808f9ba3e5fab1371c9ee82c3449a2f5eb5ed29
Zaino <hello@darrin.dev>
2022-03-15 12:35:54 -0700

    update footer

--- src/components/Footie.js
+++ src/components/Footie.js
@@ -9,10 +9,9 @@ import { Col } from 'react-bootstrap'
 import { Button } from 'react-bootstrap'
 
 const Footie = () => (
-    
-    <Row className="justify-content-md-center pt-5 pb-5">
-               this is footie
-             </Row>
+    <div className="footie">
+      <h6 className="small">© 1140nft 2022</h6>
+    </div>
 );
 
 export default Footie;
