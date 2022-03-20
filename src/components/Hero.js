324267376b977986bcc9ec52b53667b674082d62
Zaino <hello@darrin.dev>
2022-03-19 17:52:34 -0700

    misc small updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,8 +8,8 @@ import { Button } from 'react-bootstrap'
 
 const Hero = () => (
     
-    <Row className="justify-content-md-center pt-5 pb-5">
-               <Col md={{ span: 6, offset: 0 }}>
+    <Row id="hero" className="justify-content-md-center pt-5 pb-5">
+               <Col md={{ span: 4, offset: 0 }}>
                  <h1 className="1140">1140nft</h1>
                  <h3>The world's first nft funded real estate development.</h3>
                  <p>1140NFT is an innovative NFT project that shares the value of real world real estate with the NFT holders. The NFT funds the development of properties in Los Angeles into rental and resale properties. The NFT holders will have member access to the properties and share in rental and resale revenues. 
